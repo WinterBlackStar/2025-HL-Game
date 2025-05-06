78c9ff54cd297a327c08256dc8e654b9e5346955
WinterBlackStar <khanz3007@masseyhigh.school.nz>
2025-05-06 13:11:29 +1200

    Number Checker

--- C_02_ultimate_intcheck_v2.py
+++ C_02_ultimate_intcheck_v2.py
@@ -47,8 +47,15 @@ def int_check(question, low=None, high=None, exit_code=None):
 #     rounds = int_check(question="Rounds <enter> for infinite>: ", low=1, exit_code="")
 #     print(f"you asked for {rounds}")
 
-low_num = int_check("Low Number?")
-print(f"You chose a low number of {low_num}")
+# low_num = int_check("Low Number?")
+# print(f"You chose a low number of {low_num}")
 
-high_num = int_check(question="High Number?", low=1)
-print(f"You chose a high number of {high_num}")
+# high_num = int_check(question="High Number?", low=1)
+# print(f"You chose a high number of {high_num}")
+
+# Check user guesses
+guess = ""
+while guess != "xxx":
+    guess = int_check(question= "Guess: ", low=0, high=10, exit_code="xxx")
+    print(f"You guessed {guess}")
+    print()
