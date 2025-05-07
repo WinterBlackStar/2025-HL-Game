5f0251963de37e8df49888072c2b71e98dfb95c0
WinterBlackStar <khanz3007@masseyhigh.school.nz>
2025-05-07 15:00:20 +1200

    Create 06_no_duplicates.py

---
+++ 06_no_duplicates.py
@@ -0,0 +1,22 @@
+already_guessed = []
+
+secret = 7
+guesses_used = 0
+guesses_allowed = 5
+
+
+guess = ""
+while guess != secret:
+    guess = int(input("guess: "))
+
+    #check that guess is not a duplicate
+    if guess in already_guessed:
+        print(f"You've already guessed {guess}. You've *still* used "
+              f"{guesses_used} / {guesses_allowed} guesses")
+        continue
+
+    #if guess is not duplicate, add it to the 'already guessed' list
+    else:
+        already_guessed.append(guess)
+
+    guesses_used += 1
